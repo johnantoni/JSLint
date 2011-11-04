eaca59b24d0e5092a975167125f75da8d857ecb6
J Smith <dark.panda@gmail.com>
2011-11-04 11:35:35 -0400

    Fix up rhino so that it can read from STDIN and clean up in general.

--- rhino.js
+++ rhino.js
@@ -1,42 +1,74 @@
-// rhino.js
-// 2009-09-11
-/*
-Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
-*/
 
-// This is the Rhino companion to fulljslint.js.
-
-/*global JSLINT */
-/*jslint rhino: true, strict: false */
+importPackage(java.io);
+importPackage(java.lang);
 
 (function (a) {
-    var e, i, input;
-    if (!a[0]) {
-        print("Usage: jslint.js file.js");
-        quit(1);
+  function commandName() {
+    var c = environment['sun.java.command'].split(/\s+/),
+      f = new java.io.File(c[1]);
+      return f.getName();
+  }
+
+  var OPTIONS = {
+    'bitwise': true,
+    'eqeqeq': true,
+    'immed': true,
+    'newcap': true,
+    'nomen': false,
+    'onevar': true,
+    'plusplus': true,
+    'regexp': true,
+    'browser': true,
+    'undef': true,
+    'white': false,
+    'devel': true
+  };
+
+  if (a.indexOf('-h') >= 0) {
+    print("Usage: " + commandName() + " file.js OR read from STDIN");
+    quit(1);
+  }
+
+  (function() {
+    var fileName, input;
+
+    if (!a.length) {
+      (function() {
+        var reader = new BufferedReader(new InputStreamReader(System['in'])),
+          line;
+
+        input = '';
+        fileName = 'STDIN';
+
+        while ((line = reader.readLine())) {
+          input += line + "\n";
+        }
+      }());
     }
-    input = readFile(a[0]);
-    if (!input) {
-        print("jslint: Couldn't open file '" + a[0] + "'.");
-        quit(1);
+    else {
+      input = readFile(a[0]);
+      fileName = a[0];
     }
-    if (!JSLINT(input, {bitwise: true, eqeqeq: true, immed: true,
-            newcap: true, nomen: false, onevar: true, plusplus: true,
-            regexp: true, browser: true, undef: true, white: false,
-            devel: true})) {
+
+    if (!JSLINT(input, OPTIONS)) {
+      (function() {
+        var e, i;
+
+        print("ERRORS in " + fileName + ":");
         for (i = 0; i < JSLINT.errors.length; i += 1) {
-            e = JSLINT.errors[i];
-            if (e) {
-                print('Lint at line ' + e.line + ' character ' +
-                        e.character + ': ' + e.reason);
-                print((e.evidence || '').
-                        replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
-                print('');
-            }
+          e = JSLINT.errors[i];
+          if (e) {
+            print('Lint at line ' + e.line + ' character ' + e.character + ': ' + e.reason);
+            print((e.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+            print('');
+          }
         }
         quit(2);
-    } else {
-        print("jslint: No problems found in " + a[0]);
-        quit();
+      }());
     }
+    else {
+      print("SUCCESS! No problems found in " + fileName);
+      quit();
+    }
+  }());
 }(arguments));
